dff3ebed1ddcb3a4ea419b6a8a672b8caf678966
diggieman <digaholez@proton.me>
2026-07-05 09:46:11 +0700

    T812: Initial decommonize

--- Android.mk
+++ Android.mk
@@ -3,3 +3,19 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),T812)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,0f2a1441034288b407c82ef9eb0027bfa05e6e15)
+$(call add-radio-file-sha1-checked,radio/gz.img,2053cad7187a72a406506940d6061ed01a39b12e)
+$(call add-radio-file-sha1-checked,radio/lk.img,9d3cf3893f6546039fc4cd23f4444e0a7fd3f914)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,697eaf2cf41aca0de92f536b8cc84f6e14ce7f43)
+$(call add-radio-file-sha1-checked,radio/md1img.img,6fdbeaab600e35bf2887a789c495eb203376134d)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,2a6f65a09be7c1080b06218c56d35c80f40db9cc)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,1a98abe56ab8a6a19ccd9dc463e1af0d752da54c)
+$(call add-radio-file-sha1-checked,radio/scp.img,e1a73a8d097040fc9803873a871509e329cadbbe)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,4b1a3c9be2f5e019234e24cf4ff05901e71083f4)
+$(call add-radio-file-sha1-checked,radio/sspm.img,d32f42f198f40e8f96572a951dbff8783718e5a0)
+$(call add-radio-file-sha1-checked,radio/tee.img,6b5257b600c2754446e527ebd8890dc4d043326e)
+
+endif
